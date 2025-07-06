10ff5d8b1f4e16624a1c73b34809ffa43dc6470f
Greg Miller <hypurban86@gmail.com>
2025-07-05 23:43:47 -0400

    Exports the WindowManagerContext, WindowFrameContext, and WindowContext classes as they may be useful to consumers.

--- src/export.ts
+++ src/export.ts
@@ -1,7 +1,28 @@
-import WindowManager from './components/WindowManager.vue';
-import WindowFrame from './classes/WindowFrame';
+/*
+	export.ts
+	---------
 
+	defines exports for library	use
+*/
+
+// get the main component to export
+import WindowManager from './components/WindowManager.vue';
+
+// we'll want to export the frame styles, but not he class itself:
+import WindowFrame from './classes/WindowFrame';
 const FRAME_STYLE = WindowFrame.STYLE;
 
-export { WindowManager, FRAME_STYLE };
+// we'll export the context classes, for reference & type use,
+// but users probably never need to instantiate these directly:
+import WindowManagerContext from './classes/WindowManagerContext';
+import WindowFrameContext from './classes/WindowFrameContext';
+import WindowContext from './classes/WindowContext'; 
+
+export { 
+	WindowManager,
+	FRAME_STYLE,
+	WindowManagerContext,
+	WindowFrameContext,
+	WindowContext
+};
 export default WindowManager;
